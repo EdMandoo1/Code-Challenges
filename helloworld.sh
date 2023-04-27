3c8ac2ffcdd6273359b08da6a853105eed863bdf
EdMandoo1 <e.ayala03@yahoo.com>
2023-04-27 04:12:23 +0000

    I added Helloworld

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Helloworld
+# Author:                       Eduardo Ayala
+# Date of latest revision:      04/26/2023
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
+
