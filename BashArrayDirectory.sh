c158cc8d61d4d8c33b1484a5fc727a9d8fcd2550
EdMandoo1 <e.ayala03@yahoo.com>
2023-04-28 06:41:06 +0000

    Created arrays in variable and input touch. Not sure if I use  or touch .txt  need to review class video

---
+++ BashArrayDirectory.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script Name:                  Bash Array
+# Author:                       Eduardo Ayala
+# Date of latest revision:      04/27/2023
+# Purpose:                      Create 4 directories and put in an array
+
+# Declaration of variables
+directories=("dir1.txt" "dir2.txt" "dir3.txt" "dir4.txt")
+# Declaration of functions
+echo I hope this works
+touch $directories
+
+# Main
