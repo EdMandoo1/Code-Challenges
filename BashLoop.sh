656ec99e5151e19cccb63a03593032b7b0f6fc0c
EdMandoo1 <e.ayala03@yahoo.com>
2023-05-01 23:38:54 +0000

    New Bashloop file

---
+++ BashLoop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:                  Bash Loop
+# Author:                       Eduardo Ayala
+# Date of latest revision:      04/30/2023
+# Purpose:                      Create a loop
+
+# Declaration of variables
+counter=1
+# Declaration of functions
+while [ $counter -le 100 ]
+do
+echo $counter
+((counter++))
+done
+echo All done
+#Main
+# Script Name:                  Bash Loop
+# Author:                       Eduardo Ayala
+# Date of latest revision:      04/30/2023
+# Purpose:                      Create a loop
