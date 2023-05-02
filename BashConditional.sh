de0e3e19358b4f7306d147ac2b6c6a2e26e9aaa4
EdMandoo1 <e.ayala03@yahoo.com>
2023-05-02 06:42:09 +0000

    Unsure if / then is correct line 12

---
+++ BashConditional.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script Name:                  Bash Conditional Statement
+# Author:                       Eduardo Ayala
+# Date of latest revision:      05/01/2023
+# Purpose:                      To detect a file
+
+# Declaration of variables
+Sil=("SilverSurfer")
+echo "the most powerful superhero"
+# Create file for SilverSurfer
+if -e Sil [Does not exist] then touch {Sil.txt}
+
+# Declaration of functions
