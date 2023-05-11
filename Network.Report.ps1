6927b083a2ed6036051ea32b810afd78add16f5c
EdMandoo1 <e.ayala03@yahoo.com>
2023-05-11 16:54:23 +0000

    Script pulls infomation. Need to put in text file

--- Network.Report.ps1
+++ Network.Report.ps1
@@ -4,7 +4,7 @@
 # Purpose:                      Create Network report
 
 # Create network_report.txt file that holds the contents of ipconfig /all
-new-item -Path 'C:\Users\Ghost|Desktop\Network_Report.txt' -ItemType File
+ipconfig /all new-item -Path 'C:\Users\Ghost|Desktop\Network_Report.txt' 
 
 # Use Select-String to search network_report.txt and return only the IPv4 address
 Get-Content network_report.txt | Select-String -Pattern '\bIPv4 Address\b' | Out-File network_report.txt
