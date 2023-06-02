733448fa594770fa21608e888be91c9adb4553aa
EdMandoo1 <e.ayala03@yahoo.com>
2023-06-01 17:49:47 -0700

    Completed code

--- DateandTime.sh
+++ DateandTime.sh
@@ -5,15 +5,10 @@
 # Date of latest revision:      05/31/2023
 # Purpose:                      Append Date and Time
 
-# Copies /var/log/syslog to the current working directory
-# Appends the current date and time to the filename
-
-# Declaration of variables
-current_date=$(date +"%Y-%m-%d-%H:%M:%S")
-
+# cp /var/log/syslog 
+current_date=$(date +"%Y-%m-%d_%H:%M:%S")
 # Main
-echo "$(date +"%Y-%m-%d-%H:%M:%S")
 
+echo "$(date +"%Y-%m-%d-%H:%M:%S")"
 # Append Date and Time to file
-cp /var/log/syslog "~/syslog_${current_date}" 
-
+cp /var/log/syslog "syslog_$current_date" 
