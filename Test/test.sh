b77f9d322d8145e922ef2c7bdd1eec465dbedd57
EdMandoo1 <e.ayala03@yahoo.com>
2023-06-05 23:24:13 -0700

    clearing log complete

--- Test/test.sh
+++ Test/test.sh
@@ -1,46 +1,57 @@
 #!/bin/bash
 
-# Script Name:                  Conditionals in Menu Systems   
+# Script Name:                  Clearing Logs  
 # Author:                       Eduardo Ayala
-# Date of latest revision:      06/01/2023
-# Purpose:  
+# Date of latest revision:      06/05/2023
+# Purpose:  Clear log files
 
-print_menu() {
-    echo "Menu:"
-    echo "1. Hello world"
-    echo "2. Ping self"
-    echo "3. IP info"
-    echo "4. Exit"
-    echo -n "Please enter your choice: "
-}
-hello_world() {
-    echo "Hello world!"
-}
+Timestamp=$(date +%Y%m%d%H%M%S)
 
-ping_self() {
-    ping -c 4 127.0.0.1
-}
+# Directory to store compressed log files
+backup_dir="backup"
 
-ip_info() {
-    ip -c a
-}
+# Log files to compress
+log_files=(
+  "/var/log/syslog"
+  "/var/log/wtmp"
+)
 
-while true; do 
-print_menu
-read -r choice
-
-if [[ $choice == 1 ]]; then 
-hello_world
-elif [[ $choice == 2 ]]; then
-ping_self
-elif [[ $choice == 3 ]]; then 
-ip_info
-elif [[ $choice == 4 ]]; then 
-echo "Exiting..."
-sleep 
-else 
-echo "Invalid choice. Please try again. " 
-fi
-
-echo 
+# Make backup directory
+mkdir $backup_dir
+# Print file sizes before compression
+for log_file in "${log_files[@]}"; do
+  if [[ -f "$log_file" ]]; then
+    file_size=$(du -h "$log_file" | awk '{print $1}')
+    echo "File size of $log_file: $file_size"
+  else
+    echo "File $log_file does not exist."
+  fi
 done
+
+# Compress log files to backup directory and clear the contents
+for log_file in "${log_files[@]}"; do
+  if [[ -f "$log_file" ]]; then
+    base_name=$(basename "$log_file")
+    compressed_file="$backup_dir/${base_name}-${timestamp}.gz"
+    gzip -c "$log_file" > "$compressed_file"
+    compressed_size=$(du -h "$compressed_file" | awk '{print $1}')
+    echo "Compressed $log_file to $compressed_file"
+    echo "Compressed file size: $compressed_size"
+
+    original_size=$(du -h "$log_file" | awk '{print $1}')
+    echo "Original file size: $original_size"
+
+    if [[ "$compressed_size" == "$original_size" ]]; then
+      echo "Compressed file size is the same as the original file size."
+    else
+      echo "Compressed file size is different from the original file size."
+    fi
+
+    # Clear the contents of the log file
+    truncate -s 0 "$log_file"
+    echo "Cleared the contents of $log_file"
+  else
+    echo "File $log_file does not exist. Skipping compression and clearing."
+  fi
+done
+
