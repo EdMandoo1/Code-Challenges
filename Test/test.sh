401fce1c0b9539e96c939aaeb6696aa05bc07c73
EdMandoo1 <e.ayala03@yahoo.com>
2023-06-05 23:28:53 -0700

    removed test file

--- Test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script Name:                  Clearing Logs  
-# Author:                       Eduardo Ayala
-# Date of latest revision:      06/05/2023
-# Purpose:  Clear log files
-
