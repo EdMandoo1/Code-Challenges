0f888ac751aa8266878fb1b0661a09b60f784b9c
EdMandoo1 <e.ayala03@yahoo.com>
2023-06-09 10:11:15 -0700

    collections

---
+++ Python/Collections.py
@@ -0,0 +1,7 @@
+my_list = ["back_squats", "front_squats" , "leg_press", "RDL", "Lunges", " hamstring_curls"]
+
+print("First_element:", my_list[0])
+print("Second to sixth elements:", my_list[2:6])
+
+my_list[3] = "onion"
+print("Updated three element:", my_list[3])
