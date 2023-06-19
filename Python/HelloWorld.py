5a2353005ab28c36e6032e53e874f63c19b08240
EdMandoo1 <e.ayala03@yahoo.com>
2023-06-18 22:20:12 -0700

    hello world

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# Script Name:                  Hello World
+# Author:                       Eduardo Ayala
+# Date of latest revision:      06/18/23
+# Purpose:                      Python account verification
+ 
+import os
+
+# Set variable for greeting 
 greeting = "Hello World" 
+
+# Print "hello world"
 print(greeting)
-greeting.split()
