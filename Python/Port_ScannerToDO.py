5f8f6b760ea31f1ff9f2f32e3c7edfbdeee87c60
EdMandoo1 <e.ayala03@yahoo.com>
2023-09-11 09:53:21 -0700

    Made adjustments to script

--- Python/Port_ScannerToDO.py
+++ Python/Port_ScannerToDO.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
+# Script Name:                  Port Scanner tool - Todo
+# Date of latest revision:      9/11/23
+# Purpose:                      Port Scanner 
 
+#Import Libraries 
 import socket
 
 sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-timeout = # TODO: Set a timeout value here.
+
+# Set a timeout value (e.g., 1 second)
+timeout = 1
 sockmod.settimeout(timeout)
 
-hostip = # TODO: Collect a host IP from the user.
-portno = # TODO: Collect a port number from the user, then convert it to an integer data type.
+# Collect a host IP from the user
+hostip = input("Enter the host IP address: ")
+
+# Collect a port number from the user, then convert it to an integer data type
+portno = int(input("Enter the port number: "))
 
 def portScanner(portno):
-    if sockmod.FUNCTION((hostip, portno)): # TODO: Replace "FUNCTION" with the appropriate socket.function call as found in the [socket docs](https://docs.python.org/3/library/socket.html)
-        print("Port closed")
-    else:
+    try:
+        # Use the 'connect' method to check if the port is open
+        sockmod.connect((hostip, portno))
         print("Port open")
+    except socket.timeout:
+        print("Port closed (timeout)")
+    except ConnectionRefusedError:
+        print("Port closed (connection refused)")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        sockmod.close()
 
-portScanner(port)
+portScanner(portno)
